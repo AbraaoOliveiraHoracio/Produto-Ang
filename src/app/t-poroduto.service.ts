816ba5273b4cbdd6532474e2ae4eae8c2b543d39
Abraao <aohdigital@hotmail.com>
2023-05-09 18:26:17 -0300

    aa

--- src/app/t-poroduto.service.ts
+++ src/app/t-poroduto.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TPD } from './TPD';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TPorodutoService {
 
-  constructor() { }
+  constructor(private http: HttpClient){}
+
+getTPD(): Observable<TPD[]>{
+  let url = "http://localhost:3000/produto";
+  return this.http.get<TPD[]>(url);
+
+}
+
 }
--- src/app/tclient/tclient.component.ts
+++ src/app/tclient/tclient.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TPorodutoService } from '../t-poroduto.service';
 
 @Component({
   selector: 'app-tclient',
   templateUrl: './tclient.component.html',
   styleUrls: ['./tclient.component.css']
 })
-export class TClientComponent {
+export class TClientComponent implements OnInit {
+  constructor(private tporodutoService: TPorodutoService){
+
+  }
+  ngOnInit(): void {
+    this.loadProduto();
+  }
+  loadProduto(){
+    this.tporodutoService.getTPD().subscribe({
+      next : data => this.TPD = data
+    });
+
+  }
+
+
 
 }
